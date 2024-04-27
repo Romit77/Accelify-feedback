6071e7378746ea0b27bf6196f07e90e33c38b814
Romit  Dey <romit77dey@gmail.com>
2024-04-27 22:59:12 +0530

    fix v3

--- src/app/api/mail/route.ts
+++ src/app/api/mail/route.ts
@@ -5,11 +5,11 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: Request, res: Response) {
-  const { email, message, userFirstname } = await request.json();
+  const { message, userFirstname } = await request.json();
 
   const { data, error } = await resend.emails.send({
     from: "Acme <onboarding@resend.dev>",
-    to: [email],
+    to: "resourcerise17@gmail.com",
     subject: "test",
     html: render(WelcomeTemplate({ userFirstname, message })),
   });
